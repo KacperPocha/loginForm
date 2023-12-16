a3c5132de73d0197c557021fd52e155c1e5439e4
Kacper <kacper.pocha123@gmail.com>
2023-12-16 14:59:12 +0100

    Ready

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import LoginForm from "./loginForm";
+
+function App() {
+  return(
+    <LoginForm/>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/tw-elements-react/dist/js/**/*.js"
+  ],
+  theme: {
+      extend: {
+        colors: {
+          'special' : `rgba(23, 255, 14, 1)`
+        }
+      },
+  },
+  darkMode: "class",
+  plugins: [require("tw-elements-react/dist/plugin.cjs")]
+  }
